f8f3418b44c40768d4bc0bc6b78004968c04cbf6
Ashley Nygaard <ashley.d.nygaard@gmail.com>
2023-06-21 13:13:01 -0700

    create decodable quake values

---
+++ Earthquakes/Models/QuakeError.swift
@@ -0,0 +1,21 @@
+//
+//  QuakeError.swift
+//  Earthquakes-iOS
+//
+//  Created by Ashley Nygaard on 6/21/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+
+enum QuakeError: Error {
+  case missingData
+}
+
+extension QuakeError: LocalizedError {
+  var description: String? {
+    switch self {
+    case .missingData: return NSLocalizedString("Found and will discard a quake missing a valid code, magnitude, place or time", comment: "")
+    }
+  }
+}
